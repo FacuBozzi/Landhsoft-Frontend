0dc2b1569c0036b41ce97b4d1083c86eb44d29b7
facundobozzi <facundo.bo@hotmail.com>
2025-02-18 19:51:05 -0300

    updated get started button

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -162,7 +162,7 @@ const Header = () => {
             </Link> */}
 
             <Link
-              href="https://nextjstemplates.com/templates/solid"
+              href="https://cal.com"
               className="flex items-center justify-center rounded-full bg-primary px-7.5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-primaryho"
             >
               Get Started

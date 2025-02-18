049ac17ac489e5c5ae2ce6b71d628f78869c984d
facundobozzi <facundo.bo@hotmail.com>
2025-02-17 21:01:57 -0300

    trusted by companies

--- components/FunFact/index.tsx
+++ components/FunFact/index.tsx
@@ -56,12 +56,11 @@ const FunFact = () => {
             className="animate_top mx-auto mb-12.5 px-4 text-center md:w-4/5 md:px-0 lg:mb-17.5 lg:w-2/3 xl:w-1/2"
           >
             <h2 className="mb-4 text-3xl font-bold text-black dark:text-white xl:text-sectiontitle3">
-              Trusted by Global Companies.
+              Trusted by Industry Leaders.
             </h2>
             <p className="mx-auto lg:w-11/12">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-              convallis tortor eros. Donec vitae tortor lacus. Phasellus aliquam
-              ante in maximus.
+              From Toyota to Bumeran, we've crafted solutions for some of the
+              world's most respected companies.
             </p>
           </motion.div>
 
@@ -85,9 +84,9 @@ const FunFact = () => {
               className="animate_top text-center"
             >
               <h3 className="mb-2.5 text-3xl font-bold text-black dark:text-white xl:text-sectiontitle3">
-                500K
+                40+
               </h3>
-              <p className="text-lg lg:text-para2">World Wide Clients</p>
+              <p className="text-lg lg:text-para2">Companies</p>
             </motion.div>
             <motion.div
               variants={{
@@ -108,9 +107,9 @@ const FunFact = () => {
               className="animate_top text-center"
             >
               <h3 className="mb-2.5 text-3xl font-bold text-black dark:text-white xl:text-sectiontitle3">
-                1M+
+                20+
               </h3>
-              <p className="text-lg lg:text-para2">Downloads</p>
+              <p className="text-lg lg:text-para2">Years of Experience</p>
             </motion.div>
             <motion.div
               variants={{
@@ -131,9 +130,9 @@ const FunFact = () => {
               className="animate_top text-center"
             >
               <h3 className="mb-2.5 text-3xl font-bold text-black dark:text-white xl:text-sectiontitle3">
-                865
+                95%
               </h3>
-              <p className="text-lg lg:text-para2">Winning Award</p>
+              <p className="text-lg lg:text-para2">Satisfaction Rate</p>
             </motion.div>
           </div>
         </div>
